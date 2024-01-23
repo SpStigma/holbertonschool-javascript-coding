a9fa5cff1fca0b8007bd78ade46a6e5e93451378
SpStigma <yoann.rivet0@gmail.com>
2024-01-23 10:35:24 +0100

    Fix: semistandard.

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -5,22 +5,20 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, function (err, response, body) {
-    if (err) {
-        console.log('error:', err);
+  if (err) {
+    console.log('error:', err);
+  } else {
+    const obj = JSON.parse(body);
+    const tasks = {};
+    for (const task of obj) {
+      if (task.completed === true) {
+        if (tasks[task.userId] === undefined) {
+          tasks[task.userId] = 1;
+        } else {
+          tasks[task.userId] += 1;
+        }
+      }
     }
-    else {
-        const obj = JSON.parse(body);
-        const tasks = {}
-        for (const task of obj) {
-            if (task.completed === true) {
-                if (tasks[task.userId] === undefined) {
-                    tasks[task.userId] = 1;
-                }
-                else {
-                    tasks[task.userId] += 1;
-                }
-            }
-        }  
-        console.log(tasks);
-    }
-})
+    console.log(tasks);
+  }
+});
