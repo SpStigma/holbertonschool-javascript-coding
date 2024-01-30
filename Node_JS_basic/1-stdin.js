fecc7f566e206f0e5374999c7d00fbeb29f8ee3f
SpStigma <yoann.rivet0@gmail.com>
2024-01-30 10:19:12 +0100

    First attempt 1. Using Process stdin.

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,33 @@
+process.stdin.setEncoding('utf8');
+
+function getUserInput() {
+    return new Promise((resolve) => {
+        process.stdin.on('data', function(data) {
+            var chunk = data.trim();
+            if (chunk != null) {
+                resolve(chunk);
+            }
+        })
+    })
+}
+
+function endOfRead() {
+    process.stdin.on('end', function() {
+        process.stdout.write('This important software is now closing' + '\n');
+        process.exit();
+      });
+}
+
+
+async function main() {
+    console.log('Welcome to Holberton School, what is your name?')
+    const userInput = await getUserInput();
+    console.log(`Your name is: ${userInput}`);
+    endOfRead();
+
+}
+
+main()
+
+
+
