447014be9f3af70fc3f96e0899c7f2734ff53647
SpStigma <yoann.rivet0@gmail.com>
2024-01-30 13:35:16 +0100

    First attempt of 3. Reading a file asynchronously with Node JS.

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').filter(line => line.trim() !== ''); // Remove empty lines
+        const NUMBER_OF_STUDENTS = lines.length - 1;
+        console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+        const header = lines[0].split(',');
+        const rows = lines.slice(1).map(line => line.split(','));
+
+        const FirstNameCS = [];
+        const FirstNameSWE = [];
+        let NbrCs = 0;
+        let NbrSwe = 0;
+
+        rows.forEach((row) => {
+          const fieldIndex = header.indexOf('field');
+          const firstNameIndex = header.indexOf('firstname');
+          const field = row[fieldIndex];
+
+          if (field === 'CS') {
+            NbrCs += 1;
+            FirstNameCS.push(row[firstNameIndex]);
+          } else if (field === 'SWE') {
+            NbrSwe += 1;
+            FirstNameSWE.push(row[firstNameIndex]);
+          }
+        });
+
+        console.log(`Number of students in CS: ${NbrCs}. List: ${FirstNameCS.join(', ')}`);
+        console.log(`Number of students in SWE: ${NbrSwe}. List: ${FirstNameSWE.join(', ')}`);
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
